03bb1f789fd71dbbfe33d7282b92a4786b9784d2
Sijan <sijuichi1@gmail.com>
2021-08-24 05:02:52 -0700

    Personal Website

--- src/components/About.js
+++ src/components/About.js
@@ -24,7 +24,7 @@ export default function About(){
                      <a 
                       href="#projects"
                       className= "ml-4 inline-flex text-gray-100 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-pink-700 hover:text-white rounded text-lg">
-                          See My Past Work
+                          See My Past Projects
                       </a>
                 </div>
             </div>
